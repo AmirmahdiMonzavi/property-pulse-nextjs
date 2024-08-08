9ed7a8ab9ef424f025356d81015436ccc2f698e8
Amirmahdi-Monzavi <amirmahdimonzavi@gmail.com>
2024-08-08 15:53:26 +0330

    feat(layout): add metadata to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import "@/assets/styles/globals.css";
 
+export const metadata = {
+  title: "Property Pulse",
+  description: "Find the perfect rental Property",
+  keywords: "rental, property, real estate",
+};
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
