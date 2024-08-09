143f56c12b26924a907dfd3d7f3f580e0c7652de
Amirmahdi-Monzavi <amirmahdimonzavi@gmail.com>
2024-08-09 10:56:47 +0330

    feat(routes): finish home route

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -6,11 +6,15 @@ const PropertiesPage = () => {
   return (
     <section className="px-4 py-6">
       <div className="mx-auto px-4 py-6 container-xl lg:container">
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
-          {properties.map((property) => (
-            <PropertyCard key={property._id} property={property} />
-          ))}
-        </div>
+        {properties.length === 0 ? (
+          <p className="text-center">No Properties</p>
+        ) : (
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
---
+++ components/HomeProperties.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+import PropertyCard from "./PropertyCard";
+
+import properties from "@/properties.json";
+
+const HomeProperties = () => {
+  const recentProperties = properties.slice(0, 3);
+
+  return (
+    <>
+      {" "}
+      <section className="px-4 py-6">
+        <h2 className="text-center font-bold text-blue-500 text-3xl mt-6">
+          Recent Properties
+        </h2>
+        <div className="mx-auto px-4 py-6 container-xl lg:container">
+          {recentProperties.length === 0 ? (
+            <p className="text-center">No Properties</p>
+          ) : (
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+              {recentProperties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+      <section className="max-w-lg mx-auto my-10 px-6">
+        <Link
+          href="/properties"
+          className="bg-black hover:bg-gray-700 block text-white rounded-xl text-center px-6 py-4 font-bold"
+        >
+          View All Properties
+        </Link>
+      </section>
+    </>
+  );
+};
+
+export default HomeProperties;
